3eb520fc4395258be4c2d3595861067d2220d6ab
Titus Wormer <tituswormer@gmail.com>
2018-06-24 13:21:06 +0200

    Change doctype to lowercase
    Previously, The `DOCTYPE`, `SYSTEM`, and `PUBLIC` parts of the doctype
    were uppercased, however, since the rest of HTML is typically written
    lowercase, in my opinion it doesn’t make sense to use uppercase for
    only this exception where lowercase works perfectly fine.

--- test/doctype.js
+++ test/doctype.js
@@ -7,13 +7,13 @@ var to = require('..')
 test('`text`', function(t) {
   t.deepEqual(
     to(u('doctype')),
-    '<!DOCTYPE>',
+    '<!doctype>',
     'should stringify doctypes without `name`'
   )
 
   t.deepEqual(
     to(u('doctype', {name: 'html'})),
-    '<!DOCTYPE html>',
+    '<!doctype html>',
     'should stringify doctypes with `name`'
   )
 
@@ -24,7 +24,7 @@ test('`text`', function(t) {
         public: '-//W3C//DTD XHTML 1.0 Transitional//EN'
       })
     ),
-    '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN">',
+    '<!doctype html public "-//W3C//DTD XHTML 1.0 Transitional//EN">',
     'should stringify doctypes with a public identifier'
   )
 
@@ -35,7 +35,7 @@ test('`text`', function(t) {
         system: 'about:legacy-compat'
       })
     ),
-    '<!DOCTYPE html SYSTEM "about:legacy-compat">',
+    '<!doctype html system "about:legacy-compat">',
     'should stringify doctypes with a system identifier'
   )
 
@@ -47,7 +47,7 @@ test('`text`', function(t) {
         system: 'http://www.w3.org/TR/html4/strict.dtd'
       })
     ),
-    '<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//" "http://www.w3.org/TR/html4/strict.dtd">',
+    '<!doctype html public "-//W3C//DTD HTML 4.01//" "http://www.w3.org/TR/html4/strict.dtd">',
     'should stringify doctypes with both identifiers'
   )
 
@@ -58,7 +58,7 @@ test('`text`', function(t) {
         system: 'taco"'
       })
     ),
-    "<!DOCTYPE html SYSTEM 'taco\"'>",
+    "<!doctype html system 'taco\"'>",
     'should quote smartly'
   )
 
