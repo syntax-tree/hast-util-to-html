903a671d7c68f4edae58328742fb751e63cee155
Junyoung Choi <rokt33r.choi@gmail.com>
2020-01-16 19:25:39 +0900

    Add types
    Closes GH-17.
    
    Reviewed-by: Christian Murphy <christian.murphy.42@gmail.com>
    Reviewed-by: Titus Wormer <tituswormer@gmail.com>

---
+++ types/tests.ts
@@ -0,0 +1,73 @@
+import toHtml = require('hast-util-to-html')
+import {Node} from 'unist'
+
+const node: Node = {
+  type: 'element',
+  tagName: 'div'
+}
+
+const result: string = toHtml(node)
+toHtml(node, {
+  space: 'html'
+})
+toHtml(node, {
+  space: 'svg'
+})
+toHtml(node, {
+  space: 'svg'
+})
+toHtml(node, {entities: {useNamedReferences: true}})
+// $ExpectError
+toHtml(node, {entities: {escapeOnly: true}})
+// $ExpectError
+toHtml(node, {entities: {attribute: true}})
+// $ExpectError
+toHtml(node, {entities: {subset: ['subset']}})
+toHtml(node, {
+  voids: ['hr']
+})
+toHtml(node, {
+  upperDoctype: true
+})
+toHtml(node, {
+  quote: "'"
+})
+toHtml(node, {
+  quoteSmart: true
+})
+toHtml(node, {
+  preferUnquoted: true
+})
+toHtml(node, {
+  omitOptionalTags: true
+})
+toHtml(node, {
+  collapseEmptyAttributes: true
+})
+toHtml(node, {
+  closeSelfClosing: true
+})
+toHtml(node, {
+  closeEmptyElements: true
+})
+toHtml(node, {
+  tightSelfClosing: true
+})
+toHtml(node, {
+  tightCommaSeparatedLists: true
+})
+toHtml(node, {
+  tightAttributes: true
+})
+toHtml(node, {
+  tightDoctype: true
+})
+toHtml(node, {
+  allowParseErrors: true
+})
+toHtml(node, {
+  allowDangerousCharacters: true
+})
+toHtml(node, {
+  allowDangerousHTML: true
+})
