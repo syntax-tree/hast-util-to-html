e294d414e8a387ca4ffe8e9a801711a7a95dad94
Titus Wormer <tituswormer@gmail.com>
2021-05-01 16:37:38 +0200

    Remove support for non-HTML doctypes
    Related to syntax-tree/hast@2.4.0.

--- test/doctype.js
+++ test/doctype.js
@@ -2,102 +2,23 @@ import test from 'tape'
 import {u} from 'unist-builder'
 import {toHtml} from '../index.js'
 
-test('`text`', function (t) {
+test('`doctype`', function (t) {
   t.deepEqual(
     toHtml(u('doctype')),
-    '<!doctype>',
-    'should serialize doctypes without `name`'
-  )
-
-  t.deepEqual(
-    toHtml(u('doctype', {name: 'html'})),
     '<!doctype html>',
-    'should serialize doctypes with `name`'
+    'should serialize doctypes'
   )
 
   t.deepEqual(
-    toHtml(u('doctype', {name: 'html'}), {tightDoctype: true}),
+    toHtml(u('doctype'), {tightDoctype: true}),
     '<!doctypehtml>',
-    'should serialize doctypes with `name` tightly in `tightDoctype` mode'
+    'should serialize doctypes tightly in `tightDoctype` mode'
   )
 
   t.deepEqual(
-    toHtml(u('doctype', {name: 'html'}), {upperDoctype: true}),
+    toHtml(u('doctype'), {upperDoctype: true}),
     '<!DOCTYPE html>',
     'should serialize uppercase doctypes in `upperDoctype` mode'
-  )
-
-  t.deepEqual(
-    toHtml(
-      u('doctype', {
-        name: 'html',
-        public: '-//W3C//DTD XHTML 1.0 Transitional//EN'
-      })
-    ),
-    '<!doctype html public "-//W3C//DTD XHTML 1.0 Transitional//EN">',
-    'should serialize doctypes with a public identifier'
-  )
-
-  t.deepEqual(
-    toHtml(
-      u('doctype', {
-        name: 'html',
-        public: '-//W3C//DTD XHTML 1.0 Transitional//EN'
-      }),
-      {tightDoctype: true}
-    ),
-    '<!doctypehtml public"-//W3C//DTD XHTML 1.0 Transitional//EN">',
-    'should serialize doctypes with a public identifier tightly in `tightDoctype` mode'
-  )
-
-  t.deepEqual(
-    toHtml(u('doctype', {name: 'html', system: 'about:legacy-compat'})),
-    '<!doctype html system "about:legacy-compat">',
-    'should serialize doctypes with a system identifier'
-  )
-
-  t.deepEqual(
-    toHtml(u('doctype', {name: 'html', system: 'about:legacy-compat'}), {
-      tightDoctype: true
-    }),
-    '<!doctypehtml system"about:legacy-compat">',
-    'should serialize doctypes with a system identifier tightly in `tightDoctype` mode'
-  )
-
-  t.deepEqual(
-    toHtml(
-      u('doctype', {
-        name: 'html',
-        public: '-//W3C//DTD HTML 4.01//',
-        system: 'http://www.w3.org/TR/html4/strict.dtd'
-      })
-    ),
-    '<!doctype html public "-//W3C//DTD HTML 4.01//" "http://www.w3.org/TR/html4/strict.dtd">',
-    'should serialize doctypes with both identifiers'
-  )
-
-  t.deepEqual(
-    toHtml(
-      u('doctype', {
-        name: 'html',
-        public: '-//W3C//DTD HTML 4.01//',
-        system: 'http://www.w3.org/TR/html4/strict.dtd'
-      }),
-      {tightDoctype: true}
-    ),
-    '<!doctypehtml public"-//W3C//DTD HTML 4.01//""http://www.w3.org/TR/html4/strict.dtd">',
-    'should serialize doctypes with both identifiers tightly in `tightDoctype` mode'
-  )
-
-  t.deepEqual(
-    toHtml(
-      u('doctype', {
-        name: 'html',
-        system: 'taco"'
-      })
-    ),
-    "<!doctype html system 'taco\"'>",
-    'should quote smartly'
   )
 
   t.end()
