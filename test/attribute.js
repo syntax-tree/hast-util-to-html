6000808b2b9b7f418084019e9a4a095b3341bf2c
hesxenon <10255566+hesxenon@users.noreply.github.com>
2024-12-13 13:15:25 +0100

    Fix to allow other strings for boolean attributes
    Closes GH-45.
    Closes GH-46.
    
    Reviewed-by: Titus Wormer <tituswormer@gmail.com>

--- test/attribute.js
+++ test/attribute.js
@@ -161,6 +161,49 @@ test('`element` attributes', async (t) => {
         )
       }
     )
+
+    await t.test(
+      'should serialize known booleans set to arbitrary strings with value',
+      async function () {
+        assert.deepEqual(
+          toHtml(
+            h('div', {
+              selected: 'some string value for a well known boolean attribute'
+            })
+          ),
+          '<div selected="some string value for a well known boolean attribute"></div>'
+        )
+      }
+    )
+
+    await t.test(
+      'should serialize known booleans set to an empty string without value',
+      async function () {
+        assert.deepEqual(
+          toHtml(
+            h('div', {
+              selected: ''
+            })
+          ),
+          '<div selected></div>'
+        )
+      }
+    )
+
+    await t.test(
+      'should serialize known overloaded booleans set to arbitrary strings with value',
+      async function () {
+        assert.deepEqual(
+          toHtml(
+            h('div', {
+              download:
+                'some string value for a well known overloaded boolean attribute'
+            })
+          ),
+          '<div download="some string value for a well known overloaded boolean attribute"></div>'
+        )
+      }
+    )
   })
 
   await t.test('should support known overloaded booleans', async function (t) {
